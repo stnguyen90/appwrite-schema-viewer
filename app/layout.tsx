3c1944e462f0fe345b6c55d922ae28e79e1f4a3d
Steven Nguyen <1477010+stnguyen90@users.noreply.github.com>
2025-09-12 22:10:14 -0700

    chore: add logo, favicon, and og images

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,10 +7,29 @@ import './globals.css'
 
 export const metadata: Metadata = {
   title: 'Appwrite Schema Viewer',
-  description: 'Visualize your Appwrite database schema from appwrite.config.json file.',
+  description: 'Visualize your Appwrite database schema from an appwrite.config.json file.',
   icons: {
     icon: '/favicon.svg',
   },
+  openGraph: {
+    type: 'website',
+    title: 'Appwrite Schema Viewer',
+    description: 'Visualize your Appwrite database schema from an appwrite.config.json file.',
+    images: [
+      {
+        url: '/og.svg',
+        width: 1200,
+        height: 630,
+        alt: 'Appwrite Schema Viewer'
+      }
+    ]
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: 'Appwrite Schema Viewer',
+    description: 'Visualize your Appwrite database schema from an appwrite.config.json file.',
+    images: ['/og.svg']
+  }
 }
 
 export default function RootLayout({
